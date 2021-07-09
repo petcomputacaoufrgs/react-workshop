8e6fe4b9b0497eb01f4e4ce393b0188615568bbf
Eduarda Waechter <d.wchtr@gmail.com>
2021-07-09 11:07:10 -0300

    feitooooo (ou será?)

--- src/components/board/index.tsx
+++ src/components/board/index.tsx
@@ -4,17 +4,17 @@ import Piece from '../piece'
 import { HandleSwipeProps } from './props'
 import './styles.css'
 
-export const useEvent = (event: any, handler: {(this: Window, ev: any): any; (this: Window, ev: any): any;}, passive = false) => {
-    useEffect(() => {
-        window.addEventListener(event, handler, passive)
-        return () => window.removeEventListener(event, handler)
-    })
-  } 
+export const useEvent = (event: any, handler: { (this: Window, ev: any): any; (this: Window, ev: any): any; }, passive = false) => {
+  useEffect(() => {
+    window.addEventListener(event, handler, passive)
+    return () => window.removeEventListener(event, handler)
+  })
+}
 
 const SliderBoard: React.FC = () => {
-    
+
   const PIECES = 16
-  const LINES = PIECES / 4 
+  const LINES = PIECES / 4
   const TO = 0
   const FROM = 1
 
@@ -24,10 +24,10 @@ const SliderBoard: React.FC = () => {
     let freeIndexes = new Array<number>()
 
     grid.forEach((p, index) => {
-      if(p === 0) freeIndexes.push(index) 
+      if (p === 0) freeIndexes.push(index)
     })
-    
-    if(ArrayUtils.isNotEmpty(freeIndexes)) {
+
+    if (ArrayUtils.isNotEmpty(freeIndexes)) {
       let randomIndex = ArrayUtils.randomItem(freeIndexes)
       grid[randomIndex] = Math.random() >= 0.4 ? 2 : 4
     }
@@ -37,8 +37,8 @@ const SliderBoard: React.FC = () => {
 
     const moveProps: HandleSwipeProps | undefined = moveFunctions[key]
 
-    if(moveProps) {
-		handleSwipe(moveProps)
+    if (moveProps) {
+      handleSwipe(moveProps)
     }
   }
 
@@ -50,71 +50,77 @@ const SliderBoard: React.FC = () => {
   }
 
   useEffect(() => {
-	  setGameState(initialize())
+    setGameState(initialize())
   }, [])
-  
+
   useEvent("keydown", (event: KeyboardEvent) => handleKeyDown(event.key))
 
-  const handleSwipe = ({isOffline, selectLinePieces, nextPieceFrom}: HandleSwipeProps) => {
+  const handleSwipe = ({ isOffline, selectLinePieces, nextPieceFrom }: HandleSwipeProps) => {
 
-		const transfer = (array: number[], pieceTo: number, pieceFrom: number) => {
-			array[pieceTo] += array[pieceFrom]
-			array[pieceFrom] = 0
-		}
+    const transfer = (array: number[], pieceTo: number, pieceFrom: number) => {
+      array[pieceTo] += array[pieceFrom]
+      array[pieceFrom] = 0
+    }
 
-		let newArray = [...gameState]
-		let pieces = [0,1]
-		
-		for(let i = 0; i < LINES; i++) {
-			selectLinePieces(pieces, i)
-			while (!isOffline(pieces[TO], i)) {
-				if(isOffline(pieces[FROM], i)) {
-					selectLinePieces(pieces)
-				}
-				else if(newArray[pieces[FROM]] === 0) {
-					nextPieceFrom(pieces)
-				} 
-				else if(newArray[pieces[TO]] === 0 || newArray[pieces[TO]] === newArray[pieces[FROM]]) {
-					transfer(newArray, pieces[TO], pieces[FROM])
-					nextPieceFrom(pieces)
-				} 
-				else selectLinePieces(pieces)
-			}
-		}
+    let newArray = [...gameState]
+    let pieces = [0, 1]
 
-	  addNumber(newArray)
-	  setGameState([...newArray])
-	}
+    for (let i = 0; i < LINES; i++) {
+      selectLinePieces(pieces, i)
+      while (!isOffline(pieces[TO], i)) {
+        if (isOffline(pieces[FROM], i)) {
+          selectLinePieces(pieces)
+        }
+        else if (newArray[pieces[FROM]] === 0) {
+          nextPieceFrom(pieces)
+        }
+        else if (newArray[pieces[TO]] === 0 || newArray[pieces[TO]] === newArray[pieces[FROM]]) {
+          transfer(newArray, pieces[TO], pieces[FROM])
+          nextPieceFrom(pieces)
+        }
+        else selectLinePieces(pieces)
+      }
+    }
+
+    addNumber(newArray)
+    setGameState([...newArray])
+  }
 
   const handleSwipeLeft: HandleSwipeProps = {
-		isOffline: (piece, iterator) => piece > (iterator * LINES) + LINES - 1,    
-		selectLinePieces: (pieces, iterator) => {
-			pieces[TO] = iterator !== undefined ? (iterator * LINES) : (pieces[TO] + 1)
-			pieces[FROM] = pieces[TO] + 1
-		},
-		nextPieceFrom: (pieces) => pieces[FROM]++
-	}
+    isOffline: (piece, iterator) => piece > (iterator * LINES) + LINES - 1,
+    selectLinePieces: (pieces, iterator) => {
+      pieces[TO] = iterator !== undefined ? (iterator * LINES) : (pieces[TO] + 1)
+      pieces[FROM] = pieces[TO] + 1
+    },
+    nextPieceFrom: (pieces) => pieces[FROM]++
+  }
 
- const handleSwipeUp: HandleSwipeProps = {
-	isOffline: (piece, iterator) => true,
-	selectLinePieces: (pieces, iterator) => {
-	},
-	nextPieceFrom: (pieces) => {}   
- }
+  const handleSwipeUp: HandleSwipeProps = {
+    isOffline: (piece, iterator) => piece > (iterator + (LINES * (LINES - 1))),
+    selectLinePieces: (pieces, iterator) => {
+      pieces[TO] = iterator !== undefined ? iterator : (pieces[TO] + LINES)
+      pieces[FROM] = pieces[TO] + LINES
+    },
+    nextPieceFrom: (pieces) => pieces[FROM] += LINES
+  }
 
- const handleSwipeDown: HandleSwipeProps = {
-	isOffline: (piece, iterator) => true,
-	selectLinePieces: (pieces, iterator) => {
-	},
-	nextPieceFrom: (pieces) => {}
- }
+  const handleSwipeDown: HandleSwipeProps = {
+    isOffline: (piece, iterator) => piece < iterator,
+    selectLinePieces: (pieces, iterator) => {
+      pieces[TO] = iterator !== undefined ? (iterator + (LINES * (LINES - 1))) : (pieces[TO] - LINES)
+      pieces[FROM] = pieces[TO] - LINES
+    },
+    nextPieceFrom: (pieces) => pieces[FROM] -= LINES
+  }
 
- const handleSwipeRight: HandleSwipeProps = {
-	isOffline: (piece, iterator) => true,
-	selectLinePieces: (pieces, iterator) => {
-	},
-	nextPieceFrom: (pieces) => {}
- }
+  const handleSwipeRight: HandleSwipeProps = {
+    isOffline: (piece, iterator) => piece < (iterator * LINES),
+    selectLinePieces: (pieces, iterator) => {
+      pieces[TO] = iterator !== undefined ? (iterator * LINES + (LINES - 1)) : (pieces[TO] - 1)
+      pieces[FROM] = pieces[TO] - 1
+    },
+    nextPieceFrom: (pieces) => pieces[FROM]--
+  }
 
   const moveFunctions = {
     ArrowUp: handleSwipeUp,
@@ -124,11 +130,11 @@ const SliderBoard: React.FC = () => {
   }
 
   return (
-      <div className="board">
-          {gameState.map((number, index) => 
-            <Piece num={number} key={index}/>
-          )}
-      </div>
+    <div className="board">
+      {gameState.map((number, index) =>
+        <Piece num={number} key={index} />
+      )}
+    </div>
   )
 }
 
